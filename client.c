4176019c90b1cc7008f006831650e82739c4f6f7
Eatowl <csnelcov@gmail.com>
2017-01-07 22:26:55 +0700

    shared memory

---
+++ client.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+	/*
+	client code
+	*/
+	return 0;
+}
---
+++ server.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+	/*
+	server code
+	*/
+	return 0;
+}
